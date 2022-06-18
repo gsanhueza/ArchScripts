155086d3383e17c38848c7d132dffd8dd8208b23
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2022-06-18 19:48:09 -0400

    Defaulting to english/us keymap

--- env.sh
+++ env.sh
@@ -5,10 +5,10 @@ USERNAME="gabriel"              # Your username
 PASSWORD="arch"                 # Your password
 HOSTNAME="linux-pc"             # The hostname of this particular system
 USERSHELL="/bin/zsh"            # Default shell to be used by root and user
-LANGUAGE="es_CL"                # Check /etc/locale.gen for more options
-KEYMAP="la-latin1"              # Check /usr/share/kbd/keymaps/**/*.map.gz for more options
+LANGUAGE="en_US"                # Check /etc/locale.gen for more options
+KEYMAP="us"                     # Check /usr/share/kbd/keymaps/**/*.map.gz for more options
 ZONEINFO="America/Santiago"     # Check /usr/share/zoneinfo/ for more options
 DESKTOP_ENV="KDE"               # KDE, GNOME, i3, X11
 BOOTLOADER="refind"             # refind, grub
-XORG_DRIVERS="nvidia"           # nvidia, amd, vbox, intel
+XORG_DRIVERS="nvidia"           # nvidia, amd, vbox, intel, all
 
