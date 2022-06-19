1ca73e7d13c2154046963305d50c46f827db52c5
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2022-06-18 20:52:00 -0400

    Recipes are now in lowercase, comments from env.sh updated

--- env.sh
+++ env.sh
@@ -8,7 +8,6 @@ USERSHELL="/bin/zsh"            # Default shell to be used by root and user
 LANGUAGE="en_US"                # Check /etc/locale.gen for more options
 KEYMAP="us"                     # Check /usr/share/kbd/keymaps/**/*.map.gz for more options
 ZONEINFO="America/Santiago"     # Check /usr/share/zoneinfo/ for more options
-DESKTOP_ENV="KDE"               # KDE, GNOME, i3, X11
+DESKTOP_ENV="kde"               # kde, gnome, i3, x11
 BOOTLOADER="refind"             # refind, grub
 XORG_DRIVERS="nvidia"           # nvidia, amd, vbox, intel, all
-
