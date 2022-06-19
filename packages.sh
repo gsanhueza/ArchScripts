fc027926ac02bbfbca312a2958784bcdacc92acf
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2022-06-18 21:17:27 -0400

    Improved detection of packages.sh script location

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BASE_DIR=$(readlink -f ${0%/*})
+BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )"
 RECIPES_DIR="${BASE_DIR}/recipes"
 
 ALL_PACKAGES=""
@@ -8,7 +8,5 @@ ALL_PACKAGES=""
 for recipe_file in $(find ${RECIPES_DIR} -name "*.sh")
 do
     source ${recipe_file}
-    ALL_PACKAGES="${ALL_PACKAGES} ${RECIPE_PKGS}"
+    export ALL_PACKAGES="${ALL_PACKAGES} ${RECIPE_PKGS}"
 done
-
-export $ALL_PACKAGES
