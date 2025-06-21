e07bfc96602f91f27f9f332e4b96ca7568205447
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2025-06-21 12:20:50 -0400

    Updated yay_install script to send the folder to /tmp

--- yay_install.sh
+++ yay_install.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
-set -e -u
+set -eu
+
+function test_git_availability() {
+    command -v git > /dev/null 2>&1
+}
+
+function prompt_git_installation() {
+    echo "Git is not installed in the system!"
+    echo "Install it with 'sudo pacman -S git' and then re-run this script."
+    exit 1
+}
+
+function install_yay() {
+    [[ -e /tmp/yay-bin ]] && rm -rf /tmp/yay-bin
+    git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
+    makepkg --dir /tmp/yay-bin -sic --noconfirm
+}
 
 # This script will install `yay` as your AUR helper.
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si --noconfirm
-cd ..
-rm -rf yay-bin
+test_git_availability || prompt_git_installation
+install_yay
